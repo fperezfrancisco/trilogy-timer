2746da42979eec18ce2dd58584528d072ace2d59
Francisco <fperezfrancisco4@gmail.com>
2022-06-18 10:39:21 -0700

    cleaned up toggle between timer and stopwatch

--- script.js
+++ script.js
@@ -20,27 +20,32 @@ let minuteString = '';
 let miliString = '';
 
 let startMin = 0;
-let startSec = 30;
+let startSec = 10;
 
 var timeX;
+
+/** KEEP TRACK OF WHICH WATCH IS CURRENT ONE */
+
+let currentType = 'stopwatch'; // stopwatch is default starting watch
 
 /** create the stopwatch timer */
 
 function setUpStopwatch() {
+    currentType = 'stopwatch';
+
     countdowntimerBtn.classList.remove('hidden');
     stopwatchBtn.classList.add('hidden');
 
+    /*
     startBtn.removeEventListener("click", startCountdownTimer);
     startBtn.addEventListener("click", startTimer);
+    */
+    sec = 0;
+    minute = 0;
+    mili = 0;
 
     timeDisplay.textContent = "00:00:00";
     currentWatchTitle.textContent = "Stopwatch";
-}
-
-function startTimer() {
-    pauseBtn.classList.remove('hidden');
-    startBtn.classList.add('hidden');
-    timeX = setInterval(stopwatch, 10);
 }
 
 function stopwatch() {
@@ -83,38 +88,57 @@ function stopwatch() {
     timeDisplay.innerHTML = minuteString + ':' + secondString + ':' + miliString;
     mili++;   
 }
-
-function pauseTime() {
-    pauseBtn.classList.add('hidden');
-    startBtn.classList.remove('hidden');
-    clearInterval(timeX);
-}
-
-function resetTime() {
-    pauseTime();
-    sec = 0;
-    minute = 0;
-    mili = 0;
-    timeDisplay.innerHTML = '00:00:00';
-}
-
-/** Button even listeners below */
-
-startBtn.addEventListener("click", startTimer);
-pauseBtn.addEventListener("click", pauseTime);
-resetBtn.addEventListener("click", resetTime);
-
-/** FINISHED stopwatch items */
-
 /** create TIMER */
+
+function setUpTimer() {
+    currentType = 'timer';
+
+    countdowntimerBtn.classList.add('hidden');
+    stopwatchBtn.classList.remove('hidden');
+    
+    /*
+    startBtn.removeEventListener("click", startTimer);
+    startBtn.addEventListener("click", startCountdownTimer);
+
+    resetBtn.removeEventListener('click', resetTime);
+    resetBtn.addEventListener('click', resetCountdownTimer);
+    */
+
+    sec = startSec;
+    minute = startMin;
+    if (sec < 0 || minute < 0) {
+        throw console.error("invalid entries please try again");
+    }
+
+    if (sec < 10) {
+        secondString = '0' + sec;
+    } else {
+        secondString = sec;
+    }
+    if (minute < 10) {
+        minuteString = '0' + minute;
+    } else {
+        minuteString = minute;
+    }
+
+    timeDisplay.textContent = minuteString + ':' + secondString;
+    currentWatchTitle.textContent = "Timer";
+}
 
 function countdownTimer() {
     /**default timer time */
+    sec--;
     if (sec <= 0) {
         /** create alert that timer is done */
         if (minute <= 0) {
-            /** timer done */
-            return window.alert("TIMER DONE!");
+            /** timer done */         
+            minuteString = '0' + minute;
+            secondString = '0' + sec;
+            timeDisplay.innerHTML = minuteString + ':' + secondString;
+            currentWatchTitle.textContent = 'TIMER DONE!!';
+            currentWatchTitle.style.fontSize = '3rem'; 
+            pauseTime();
+            return setTimeout(resetCountdownTimer, 5000);
         } else {
             minute--;
             sec = 59;
@@ -132,35 +156,44 @@ function countdownTimer() {
     } else {
         secondString = sec;
     }
-
     timeDisplay.innerHTML = minuteString + ':' + secondString;
-    sec--;
-
 }
 
-function startCountdownTimer() {
+/** play, pause, and reset functionality */
+
+function startTimer() {
     pauseBtn.classList.remove('hidden');
     startBtn.classList.add('hidden');
 
-    timeX = setInterval(countdownTimer, 1000);
-}
-
-function setUpTimer() {
-    countdowntimerBtn.classList.add('hidden');
-    stopwatchBtn.classList.remove('hidden');
-
-    startBtn.removeEventListener("click", startTimer);
-    startBtn.addEventListener("click", startCountdownTimer);
-
-    sec = startSec;
-    minute = startMin;
-    if (sec < 0 || minute < 0) {
-        throw console.error("invalid entries please try again");
+    if (currentType === 'stopwatch') {
+        timeX = setInterval(stopwatch, 10);
+    } else if (currentType === 'timer') {
+        timeX = setInterval(countdownTimer, 1000); 
     }
-
-    timeDisplay.textContent = "00:30";
-    currentWatchTitle.textContent = "Timer";
 }
+
+function pauseTime() {
+    pauseBtn.classList.add('hidden');
+    startBtn.classList.remove('hidden');
+    clearInterval(timeX);
+}
+
+function resetTime() {
+    pauseTime();
+    if (currentType === 'stopwatch') {
+       setUpStopwatch();
+    } else if (currentType === 'timer') {
+        setUpTimer();
+    }
+}
+
+/** Button even listeners below */
+
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", pauseTime);
+resetBtn.addEventListener("click", resetTime);
+
+/** FINISHED watch items */
 
 countdowntimerBtn.addEventListener('click', setUpTimer);
 stopwatchBtn.addEventListener('click', setUpStopwatch);
